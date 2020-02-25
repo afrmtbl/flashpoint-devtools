bb889872f3d7dffae0de4e38f197682c92ba01df
afrmtbl <afrmtbl@gmail.com>
2020-02-25 15:40:23 -0500

    Add validator tab

--- devtools.py
+++ devtools.py
@@ -2,9 +2,7 @@ import tkinter as tk
 import tkinter.ttk as ttk
 
 from src.tabs.metadata_editor import MetadataEditorTab
-
-# not complete yet
-# from src.tabs.missing_files import MissingFilesTab
+from src.tabs.metadata_validator import MetadataValidatorTab
 
 WINDOW_WIDTH = 460
 WINDOW_HEIGHT = 200
@@ -15,8 +13,8 @@ root.title("Flashpoint DevTools")
 root.iconbitmap("icon.ico")
 
 root.minsize(WINDOW_WIDTH, WINDOW_HEIGHT)
-root.maxsize(WINDOW_WIDTH, WINDOW_HEIGHT)
-root.resizable(False, False)
+# root.maxsize(WINDOW_WIDTH, WINDOW_HEIGHT)
+root.resizable(True, True)
 
 root.rowconfigure(0, weight=1)
 root.columnconfigure(0, weight=1)
@@ -28,10 +26,10 @@ style.configure("MY.TLabel", background="white")
 note = ttk.Notebook(root)
 
 tab1 = MetadataEditorTab(note, style="MY.TFrame")
-# tab2 = MissingFilesTab(note, style="MY.TFrame")
+tab2 = MetadataValidatorTab(note, style="MY.TFrame")
 
 note.add(tab1, text="Metadata Editor", padding="24px 0px")
-# note.add(tab2, text="Missing Files", padding="24px 0px")
-note.grid(sticky=tk.NW+tk.SE)
+note.add(tab2, text="Metadata Validator", padding="24px 0px")
+note.grid(sticky=tk.NW + tk.SE)
 
 root.mainloop()
