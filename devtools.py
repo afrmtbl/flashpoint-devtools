abbef81e8a031e365224d948bcd7fe5597b9f787
afrmtbl <afrmtbl@gmail.com>
2020-02-26 03:10:29 -0500

    Revert "Merge pull request #2 from afrmtbl/validator"
    This reverts commit a742f13a04a7ce81cb0d23c0fa0487afdaf41d5b, reversing
    changes made to 97a1186e4ae215f896ee72ead47818c66003b45d.

--- devtools.py
+++ devtools.py
@@ -2,7 +2,9 @@ import tkinter as tk
 import tkinter.ttk as ttk
 
 from src.tabs.metadata_editor import MetadataEditorTab
-from src.tabs.metadata_validator import MetadataValidatorTab
+
+# not complete yet
+# from src.tabs.missing_files import MissingFilesTab
 
 WINDOW_WIDTH = 460
 WINDOW_HEIGHT = 200
@@ -13,8 +15,8 @@ root.title("Flashpoint DevTools")
 root.iconbitmap("icon.ico")
 
 root.minsize(WINDOW_WIDTH, WINDOW_HEIGHT)
-# root.maxsize(WINDOW_WIDTH, WINDOW_HEIGHT)
-root.resizable(True, True)
+root.maxsize(WINDOW_WIDTH, WINDOW_HEIGHT)
+root.resizable(False, False)
 
 root.rowconfigure(0, weight=1)
 root.columnconfigure(0, weight=1)
@@ -26,10 +28,10 @@ style.configure("MY.TLabel", background="white")
 note = ttk.Notebook(root)
 
 tab1 = MetadataEditorTab(note, style="MY.TFrame")
-tab2 = MetadataValidatorTab(note, style="MY.TFrame")
+# tab2 = MissingFilesTab(note, style="MY.TFrame")
 
 note.add(tab1, text="Metadata Editor", padding="24px 0px")
-note.add(tab2, text="Metadata Validator", padding="24px 0px")
-note.grid(sticky=tk.NW + tk.SE)
+# note.add(tab2, text="Missing Files", padding="24px 0px")
+note.grid(sticky=tk.NW+tk.SE)
 
 root.mainloop()
