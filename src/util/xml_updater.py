d9f38d5266c065824770c12d5dfdfa3e46d5378f
afrmtbl <afrmtbl@gmail.com>
2020-03-02 03:49:58 -0500

    Finish additional app support + fixes and cleanup

--- src/util/xml_updater.py
+++ src/util/xml_updater.py
@@ -1,3 +1,5 @@
+"""Handles parsing the YAML changes files and actually applying the changes to an XML file."""
+
 from lxml import etree as ET
 import yaml
 import uuid
@@ -5,46 +7,58 @@ import uuid
 from typing import Dict, Union, Tuple, Optional
 
 aliased_keys = {
-    "Extreme": "Hide",
+    "Application Path": "ApplicationPath",
     "Launch Command": "CommandLine",
+
+    "Extreme": "Hide",
     "Note": "Notes",
     "Languages": "Language",
-
-    "Application Path": "ApplicationPath"
+    "Release Date": "ReleaseDate",
+    "Alternate Titles": "AlternateTitles",
+    "Play Mode": "PlayMode",
+    "Original Description": "OriginalDescription"
 }
 
 
-# XML updating exceptions
-class GameNotFound(Exception):
-    def __init__(self, message, game_id):
-        super().__init__(message)
-        self.game_id = game_id
+class UpdaterExceptions:
+
+    # XML updating exceptions
+    class GameNotFound(Exception):
+        def __init__(self, message, game_id):
+            super().__init__(message)
+            self.game_id = game_id
+
+    class MissingElement(Exception):
+        def __init__(self, message, game_id, element_name):
+            super().__init__(message)
+            self.game_id = game_id
+            self.element_name = element_name
+
+    class MissingElementValue(Exception):
+        pass
+
+    # Parser exceptions
+    class InvalidGameId(Exception):
+        pass
+
+    class ForbiddenElementChange(Exception):
+        pass
 
 
-class MissingGameId(Exception):
-    def __init__(self, message, game_id):
-        super().__init__(message)
-        self.game_id = game_id
+def process_yaml_value(value):
+    processed_val = None
 
+    if isinstance(value, dict):
+        raise UpdaterExceptions.ForbiddenElementChange()
 
-class MissingElement(Exception):
-    def __init__(self, message, game_id, element_name):
-        super().__init__(message)
-        self.game_id = game_id
-        self.element_name = element_name
+    elif isinstance(value, list):
+        processed_val = "; ".join([process_yaml_value(el) for el in value])
+    elif isinstance(value, bool):
+        processed_val = str(value).lower()
+    else:
+        processed_val = str(value)
 
-
-class MissingElementValue(Exception):
-    pass
-
-
-# Parser exceptions
-class InvalidGameId(Exception):
-    pass
-
-
-class ForbiddenElementChange(Exception):
-    pass
+    return processed_val
 
 
 def process_yaml(yaml_document: Dict, is_additional_application: bool = False):
@@ -67,20 +81,11 @@ def process_yaml(yaml_document: Dict, is_additional_application: bool = False):
         key_alias = aliased_keys.get(key, key)
         val = yaml_document[key]
 
-        processed_val = None
-
-        if isinstance(val, dict):
+        try:
+            yaml_document[key_alias] = process_yaml_value(val)
+        except ForbiddenElementChange:
             prefix = f"'{key_alias}' and '{key}'" if key_alias != key else f"'{key}'"
-            raise ForbiddenElementChange(f"{prefix} cannot have an object as a value")
-
-        elif isinstance(val, list):
-            processed_val = "; ".join(val)
-        elif isinstance(val, bool):
-            processed_val = str(val).lower()
-        else:
-            processed_val = str(val)
-
-        yaml_document[key_alias] = processed_val
+            raise UpdaterExceptions.ForbiddenElementChange(f"{prefix} cannot have an object as a value")
 
         if key_alias != key:
             del yaml_document[key]
@@ -88,7 +93,12 @@ def process_yaml(yaml_document: Dict, is_additional_application: bool = False):
     if not is_additional_application and "Additional Applications" in static_keys:
         additional_apps = yaml_document["Additional Applications"]
         for app in additional_apps:
-            process_yaml(additional_apps[app], True)
+            app_val = additional_apps[app]
+
+            if isinstance(app_val, dict):
+                process_yaml(additional_apps[app], True)
+            else:
+                additional_apps[app] = process_yaml_value(app_val)
 
     return yaml_document
 
@@ -101,10 +111,10 @@ def parse_changes_file(file_path: str) -> Dict:
     with open(file_path, "r", encoding="utf8") as changes_file:
         for index, document in enumerate(yaml.safe_load_all(changes_file.read())):
             if "GAME" not in document:
-                raise InvalidGameId(f"Document {index} is missing a \'GAME\' entry")
+                raise UpdaterExceptions.InvalidGameId(f"Document {index} is missing a \'GAME\' entry")
 
             if "ID" in document:
-                raise ForbiddenElementChange("The \'ID\' element cannot be modified")
+                raise UpdaterExceptions.ForbiddenElementChange("The \'ID\' element cannot be modified")
 
             game_id = document["GAME"]
             del document["GAME"]
@@ -131,11 +141,11 @@ def try_get_element(element_name: str, root: ET.Element, get_text: bool = False,
             if el_text or not raise_on_no_text:
                 return el, el_text
             else:
-                raise MissingElementValue(f"'{el.tag}' element inside '{root.tag}' is missing a value")
+                raise UpdaterExceptions.MissingElementValue(f"'{el.tag}' element inside '{root.tag}' is missing a value")
         else:
             return el
     else:
-        raise MissingElement(f"Element '{root.tag}' is missing a '{element_name}' element", None, None)
+        raise UpdaterExceptions.MissingElement(f"Element '{root.tag}' is missing a '{element_name}' element", None, None)
 
 
 def update_xml_element(xml_root: ET.Element, element: ET.Element, changes: dict, game_id: str, create_elements_whitelist: list, is_additional_application: bool = False):
@@ -161,7 +171,28 @@ def update_xml_element(xml_root: ET.Element, element: ET.Element, changes: dict,
             created_el.text = value
         else:
             error_text: str = f"{game_id} is missing element: \'{key}\'. If you'd prefer the element be created instead, add it on a new line in the elements whitelist (elements_whitelist.txt)"
-            raise MissingElement(error_text, game_id, key)
+            raise UpdaterExceptions.MissingElement(error_text, game_id, key)
+
+
+def create_additional_application(xml_root: ET.Element, game_id: str, app_name: str, application_path: str, command_line: str) -> ET.Element:
+    new_add_app_el = ET.SubElement(xml_root, "AdditionalApplication")
+
+    children = {
+        "Id": str(uuid.uuid4()),
+        "GameID": game_id,
+        "Name": app_name,
+
+        "ApplicationPath": application_path,
+        "CommandLine": command_line,
+
+        "AutoRunBefore": "false",
+        "WaitForExit": "false"
+    }
+
+    elements_whitelist = list(children.keys())
+    update_xml_element(xml_root, new_add_app_el, children, game_id, elements_whitelist, True)
+
+    return new_add_app_el
 
 
 def handle_additional_apps(xml_root: ET.Element, game_id: str, changes: dict, create_elements_whitelist: list):
@@ -174,6 +205,7 @@ def handle_additional_apps(xml_root: ET.Element, game_id: str, changes: dict, cr
     found_apps: Dict[str, ET.Element] = {}
 
     for app in xml_root.iter("AdditionalApplication"):
+
         app_game_id = try_get_element("GameID", app, get_text=True, raise_on_no_text=True)[1]
 
         if app_game_id == game_id:
@@ -181,40 +213,44 @@ def handle_additional_apps(xml_root: ET.Element, game_id: str, changes: dict, cr
             found_apps[app_name] = app
 
     for app_name in changes:
+        # If the additional application already exists
         if app_name in found_apps:
             changes_list = changes[app_name]
             app_element = found_apps[app_name]
 
             app_id = try_get_element("Id", app_element, True, True)[1]
 
-            update_xml_element(xml_root, app_element, changes_list, app_id, create_elements_whitelist, True)
-        else:
-            # TODO: handle :extras: and :message:
+            if isinstance(changes_list, dict):
+                update_xml_element(xml_root, app_element, changes_list, app_id, create_elements_whitelist, True)
+            elif isinstance(changes_list, str) and (app_name == "Extras" or app_name == "Message"):
+                changes_list = {"CommandLine": changes_list}
+                update_xml_element(xml_root, app_element, changes_list, app_id, create_elements_whitelist, True)
+            else:
+                msg = f"Inside of the 'Additional Applications' key, only 'Extras' and 'Message' can have a string as a value"
+                raise UpdaterExceptions.ForbiddenElementChange(msg)
 
+        else:
+            # In the case of `Extras: str` and `Message: str`, this will be a string
             changes_list = changes[app_name]
 
-            if "ApplicationPath" not in changes_list:
-                raise MissingElementValue("The 'ApplicationPath' or 'Application Path' key must be included in the metadata edit in order to create a new Additional Application")
-            elif "CommandLine" not in changes_list:
-                raise MissingElementValue("The 'CommandLine' or 'Launch Command' key must be included in the metadata edit in order to create a new Additional Application")
+            if isinstance(changes_list, dict):
+                if "ApplicationPath" not in changes_list:
+                    raise UpdaterExceptions.MissingElementValue(f"{app_name}: The 'ApplicationPath' or 'Application Path' key must be included in the metadata edit in order to create a new Additional Application")
+                elif "CommandLine" not in changes_list:
+                    raise UpdaterExceptions.MissingElementValue(f"{app_name}: The 'CommandLine' or 'Launch Command' key must be included in the metadata edit in order to create a new Additional Application")
 
-            new_add_app_el = ET.SubElement(xml_root, "AdditionalApplication")
+                application_path = changes_list["ApplicationPath"]
+                command_line = changes_list["CommandLine"]
 
-            children = {
-                "Id": str(uuid.uuid4()),
-                "GameID": game_id,
-                "Name": app_name,
+                new_add_app_el = create_additional_application(xml_root, game_id, app_name, application_path, command_line)
+                update_xml_element(xml_root, new_add_app_el, changes_list, game_id, create_elements_whitelist, True)
 
-                "ApplicationPath": changes_list["ApplicationPath"],
-                "CommandLine": changes_list["CommandLine"],
-
-                "AutoRunBefore": "false",
-                "WaitForExit": "false"
-            }
-
-            elements_whitelist = list(children.keys())
-            update_xml_element(xml_root, new_add_app_el, children, game_id, elements_whitelist, True)
-            update_xml_element(xml_root, new_add_app_el, changes_list, game_id, create_elements_whitelist, True)
+            elif isinstance(changes_list, str) and (app_name == "Extras" or app_name == "Message"):
+                application_path = ":extras:" if changes_list == "Extras" else ":message:"
+                new_add_app_el = create_additional_application(xml_root, game_id, app_name, application_path, changes_list)
+            else:
+                msg = f"Inside of the 'Additional Applications' key, only 'Extras' and 'Message' can have a string as a value"
+                raise UpdaterExceptions.ForbiddenElementChange(msg)
 
 
 def get_updated_xml(changes: dict, source_xml_path: str, create_elements_whitelist: list):
@@ -239,9 +275,52 @@ def get_updated_xml(changes: dict, source_xml_path: str, create_elements_whiteli
 
     for game_id in changes:
         if game_id not in results:
-            raise GameNotFound(f"Unable to find game: \'{game_id}\'", game_id)
+            raise UpdaterExceptions.GameNotFound(f"Unable to find game: \'{game_id}\'", game_id)
 
-    return tree
+    return tree, results
+
+
+def explain_changes(all_changes: dict, is_additional_application: bool = False):
+    explanation = ""
+    spacing = "      "
+
+    def changes_to_str(changes: dict, is_additional_application: bool = False):
+        explanation = ""
+        for element_name in changes:
+
+            val = changes[element_name]
+
+            if element_name == "Additional Applications" and not is_additional_application:
+                for app_name in val:
+                    app_changes = val[app_name]
+
+                    if isinstance(app_changes, str):
+                        line = f"Additional application \"{app_name}\" (created or modified)\n"
+                        line += (spacing * 2) + f"\"CommandLine\" element now has a value of \"{app_changes}\"\n"
+                        explanation += "\n" + spacing + line
+                    elif isinstance(app_changes, dict):
+                        line = f"Additional application \"{app_name}\" (created or modified)\n"
+                        line += changes_to_str(app_changes, is_additional_application=True)
+                        explanation += "\n" + spacing + line
+                    else:
+                        raise Exception(f"Invalid additional application value ({app_name}, {app_changes})")
+            else:
+                line = f"\"{element_name}\" element was changed to \"{val}\"\n"
+                cur_spacing = 2 * spacing if is_additional_application else spacing
+                suffix = "" if is_additional_application else "\n"
+                explanation += suffix + cur_spacing + line
+
+        return explanation
+
+    for game_id in all_changes:
+        explanation += game_id + "\n"
+        game_changes = all_changes[game_id]
+
+        explanation += changes_to_str(game_changes)
+
+        explanation += "\n"
+
+    return explanation
 
 
 if __name__ == "__main__":
@@ -249,10 +328,14 @@ if __name__ == "__main__":
     home = str(Path.home())
 
     changes = parse_changes_file(f"{home}/Desktop/changes.yml")
-    import json
-    print(json.dumps(changes, indent=4))
-    updated_xml = get_updated_xml(changes, f"{home}/Desktop/Unity.xml", ["Hide"])
+    exp = explain_changes(changes)
 
-    updated_xml.write("updated.xml", encoding="utf8", pretty_print=True)
+    print(exp)
+    # import json
+    # print(json.dumps(changes, indent=4))
+    # updated_xml, changed_games = get_updated_xml(changes, f"{home}/Desktop/Unity.xml", ["Hide"])
+    # print(changed_games)
+
+    # updated_xml.write("updated.xml", encoding="utf8", pretty_print=True)
 
     # print(ET.tostring(updated_xml.getroot(), method="xml", encoding="unicode", pretty_print=True))
