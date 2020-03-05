692289a37915272e10771ef5e68b6cc8baa314fc
afrmtbl <afrmtbl@gmail.com>
2020-03-05 03:06:01 -0500

    get_updated_xml now returns changed games as well as failed games

--- src/util/xml_updater.py
+++ src/util/xml_updater.py
@@ -6,11 +6,12 @@ import uuid
 
 import re
 
-from typing import Dict, Union, Tuple, Optional
+from typing import Dict, Union, Tuple, Optional, List
 
 aliased_keys = {
     "Application Path": "ApplicationPath",
     "Launch Command": "CommandLine",
+    "Command Line": "CommandLine",
 
     "Extreme": "Hide",
     "Note": "Notes",
@@ -229,11 +230,7 @@ class XmlUpdater:
         return new_add_app_el
 
     def handle_additional_apps(self, xml_root: ET.Element, game_id: str, changes: dict, create_elements_whitelist: list):
-        """
-        Handles creating new and updating existing additional applications.
-
-        TODO: Special handling will be included for :extras: and :message: applications.
-        """
+        """Handles creating new and updating existing additional applications."""
 
         found_apps: Dict[str, ET.Element] = {}
 
@@ -287,7 +284,7 @@ class XmlUpdater:
                     msg = f"Inside of the 'Additional Applications' key, only 'Extras' and 'Message' can have a string as a value"
                     raise self.ForbiddenElementChange(msg, self.current_game_id, None)
 
-    def get_updated_xml(self, changes: dict, source_xml_path: str, create_elements_whitelist: list, raise_on_missing_game: bool = True):
+    def get_updated_xml(self, changes: dict, source_xml_path: str, create_elements_whitelist: list) -> Tuple[ET.Element, set, list]:
         """Wrapper function for `update_xml_element` that simplifies applying changes to an XML file."""
 
         parser = ET.XMLParser(remove_blank_text=True)
@@ -295,7 +292,8 @@ class XmlUpdater:
         root = tree.getroot()
         # Collect game IDs that were successfully changed
         # so we can compare against the ones specified in the changes file
-        results: set = set()
+        games_changed: set = set()
+        games_failed: List[Exception] = []
 
         for game in root.iter("Game"):
             game_id_element, game_id = self.try_get_element("ID", game, True, True)
@@ -303,20 +301,16 @@ class XmlUpdater:
             if game_id and game_id in changes:
                 changes_list = changes[game_id]
                 self.current_game_id = game_id
-                self.update_xml_element(root, game, changes_list, game_id, create_elements_whitelist)
+                try:
+                    self.update_xml_element(root, game, changes_list, game_id, create_elements_whitelist)
+                    games_changed.add(game_id)
+                except Exception as e:
+                    games_failed.append(e)
 
-                # Mark the game as successfully changed
-                results.add(game_id)
-
-        if raise_on_missing_game:
-            for game_id in changes:
-                if game_id not in results:
-                    raise self.GameNotFound(f"Unable to find game: \'{game_id}\'", game_id)
-
-        return tree, results
+        return tree, games_changed, games_failed
 
 
-def explain_changes(all_changes: dict, is_additional_application: bool = False):
+def explain_changes(all_changes: dict, is_additional_application: bool = False) -> str:
     explanation = ""
     spacing = "      "
 
@@ -366,14 +360,15 @@ if __name__ == "__main__":
     changes = ChangesParser.parse_changes_file(f"{home}/Desktop/changes.yml")
     exp = explain_changes(changes)
 
-    print(exp)
+    # print(exp)
     # import json
     # print(json.dumps(changes, indent=4))
     updater = XmlUpdater()
-    try:
-        updated_xml, changed_games = updater.get_updated_xml(changes, f"{home}/Desktop/Unity.xml", ["Hide"])
-    except Exception as e:
-        print(str(e), e.game_id)
+
+    updated_xml, games_changed, games_failed = updater.get_updated_xml(changes, f"{home}/Desktop/platform_xmls/Flash.xml", ["Tags", "OriginalDescription", "ReleaseDate"])
+    print(games_changed)
+
+    print(games_failed)
 
     # print(changed_games)
 
